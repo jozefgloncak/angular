26a9a65ce9cb9856ecb3efd340002224d4102d2d
Jozef Gloncak <jozef.gloncak@gmail.com>
2017-12-15 13:00:48 +0100

    display user in standalone component.
    Signed-off-by: Jozef Gloncak <jozef.gloncak@gmail.com>

--- users-from-DB/src/app/app.module.ts
+++ users-from-DB/src/app/app.module.ts
@@ -2,22 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { MatTableModule } from '@angular/material/table';
+import { MatHeaderCell } from '@angular/material/table';
+import { MatCell } from '@angular/material/table';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { DisplayUsersComponent } from './display-users/display-users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayUsersComponent
   ],
   imports: [
     BrowserAnimationsModule,
     MatButtonModule,
-    MatMenuModule
+    MatMenuModule,
+    MatTableModule
   ],
   exports: [
     MatButtonModule,
-    MatMenuModule
+    MatMenuModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
