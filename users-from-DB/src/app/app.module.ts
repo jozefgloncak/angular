8b7407e1badcea4f3174499db810db8e9a78eaa2
Jozef Gloncak <jozef.gloncak@gmail.com>
2017-12-15 12:59:47 +0100

    Menu with submenu.
    Signed-off-by: Jozef Gloncak <jozef.gloncak@gmail.com>

--- users-from-DB/src/app/app.module.ts
+++ users-from-DB/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
